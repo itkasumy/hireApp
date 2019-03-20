b09b7f7abf92f01e2656b0028468bf4d966dc918
kasumy <18500682038@163.com>
2019-03-20 23:20:36 +0800

    创建项目

--- src/index.js
+++ src/index.js
@@ -1,12 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import { createStore, applyMiddleware, compose } from 'redux';
+import thunk from 'redux-thunk';
+import { Provider } from 'react-redux'
+import { BrowserRouter, Route, Switch } from 'react-router-dom'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import reducer from './reducer';
+import './config'
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+const chromeReduxDevtools = window.__REDUX_DEVTOOLS_EXTENSION__ ? window.__REDUX_DEVTOOLS_EXTENSION__ : () => { }
+
+const store = createStore(reducer, compose(
+  applyMiddleware(thunk),
+  chromeReduxDevtools()
+))
+
+ReactDOM.render(
+  <Provider store={store}>
+    <BrowserRouter>
+      <Switch>
+        <Route path="/login" />
+        <Route path="/dashboard" />
+      </Switch>
+    </BrowserRouter>
+  </Provider>,
+  document.getElementById('root'))

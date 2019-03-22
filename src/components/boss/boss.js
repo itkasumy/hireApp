91cedd6eb076668c2a16cd1f0374859814054e39
kasumy <18500682038@163.com>
2019-03-22 17:20:32 +0800

    数据添加到redux中

--- src/components/boss/boss.js
+++ src/components/boss/boss.js
@@ -1,7 +1,12 @@
 import React from 'react'
-import axios from 'axios'
 import { Card, WhiteSpace, WingBlank } from 'antd-mobile'
+import { connect } from 'react-redux'
+import { getUserList } from '../../redux/chatuser';
 
+@connect(
+  state => state.chatuser,
+  {getUserList}
+)
 class Boss extends React.Component {
   constructor(props) {
     super(props);
@@ -11,14 +16,16 @@ class Boss extends React.Component {
   }
   
   componentDidMount() {
-    axios.get('/user/list?type=boss')
-      .then(res => {
-        if (res.status === 200 && res.data.code === 0) {
-          this.setState({
-            data: res.data.data
-          })
-        }
-      })
+  //   axios.get('/user/list?type=boss')
+  //     .then(res => {
+  //       if (res.status === 200 && res.data.code === 0) {
+  //         this.setState({
+  //           data: res.data.data
+  //         })
+  //       }
+  //     })
+    this.props.getUserList('genius')
+    console.log(this.props)
   }
 
   render() {
@@ -26,7 +33,7 @@ class Boss extends React.Component {
     return (
       <WingBlank>
         {
-          this.state.data.map(v => v.avatar ? (
+          this.props.userlist.map(v => v.avatar ? (
             <div key={v._id}>
               <Card>
                 <Card.Header

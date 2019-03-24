2a696cebf234505b0fe28fb61871167de75c0a00
kasumy <18500682038@163.com>
2019-03-25 02:20:50 +0800

    最新消息排序

--- src/components/msg/msg.js
+++ src/components/msg/msg.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { connect } from 'react-redux'
-import { List } from 'antd-mobile'
+import { List, Badge } from 'antd-mobile'
 
 @connect(
   state => state
@@ -24,17 +24,26 @@ class Msg extends React.Component {
       msgGroup[v.chatid] = msgGroup[v.chatid] || []
       msgGroup[v.chatid].push(v) 
     })
-    const chatList = Object.values(msgGroup)
+    const chatList = Object.values(msgGroup).sort((a, b) => {
+      const a_last = this.getLast(a).create_time
+      const b_last = this.getLast(b).create_time
+      return b_last - a_last
+    })
 
-    console.log(chatList)
     return (<div>
       {chatList.map(v => {
         const lastItem = this.getLast(v)
         const targetId = v[0].from === userid ? v[0].to : v[0].from
         const name = userinfo[targetId] ? userinfo[targetId].name : ''
         const avatar = userinfo[targetId] ? userinfo[targetId].avatar : ''
+        const unreadNum = v.filter(val => !val.read && userid === val.to).length
         return (<List key={lastItem._id}>
-          <Item thumb={avatar ? require(`../img/${avatar}.png`) : ''}>
+          <Item
+            thumb={avatar ? require(`../img/${avatar}.png`) : ''}
+            extra={<Badge text={unreadNum}></Badge>}
+            arrow="horizontal"
+            onClick={() => this.props.history.push(`/chat/${targetId}`)}
+          >
             {lastItem.content}:
             <Brief>{name}</Brief>
           </Item>
